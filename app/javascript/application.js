22a64a40c6227d2dde25dfea51fbc02808f50c2f
Harsh <harshpatel.backend.dev@gmail.com>
2026-06-14 00:39:23 +0530

    hotfixes

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,4 @@
 import "@hotwired/turbo-rails"
 import "@hotwired/stimulus"
-import * as ActionCable from "@rails/actioncable"
-window.ActionCable = ActionCable
-import "./controllers"
 import "./channels/index"
+import "./controllers"
