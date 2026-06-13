ff392ffebc26558a6672e704afaeacb726017d86
Harsh <harshpatel.backend.dev@gmail.com>
2026-06-13 19:12:25 +0530

    hotfixes

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,19 +1,11 @@
 # config/importmap.rb
-# Pin your main application
 pin "application", preload: true
-
-# Pin Rails defaults
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-
-# Pin ActionCable
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "@rails/actioncable", to: "actioncable.esm.js"
 
-pin_all_from "app/javascript/channels", under: "channels"
-
-# Pin all controllers
-pin_all_from "app/javascript/controllers", under: "controllers"
-
-# If you have any other directories, pin them too
-# pin_all_from "app/javascript/custom", under: "custom"
+# Explicitly pin each file instead of using pin_all_from
+pin "channels/auction_room_channel", to: "channels/auction_room_channel.js"
+pin "controllers/application", to: "controllers/application.js"
+pin "controllers/hello_controller", to: "controllers/hello_controller.js"
