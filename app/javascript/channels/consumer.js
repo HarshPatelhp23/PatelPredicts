3a20d4003c9cb1f9267f4c09825ff7000f507d9d
Harsh <harshpatel.backend.dev@gmail.com>
2026-06-14 00:32:30 +0530

    hotfixes

--- app/javascript/channels/consumer.js
+++ app/javascript/channels/consumer.js
@@ -1,5 +1,2 @@
 import { createConsumer } from "@rails/actioncable"
-import ActionCable from "@rails/actioncable"
-const consumer = ActionCable.createConsumer()
-window._actionCableConsumer = window._actionCableConsumer || createConsumer()
-export default window._actionCableConsumer
+export default createConsumer()
