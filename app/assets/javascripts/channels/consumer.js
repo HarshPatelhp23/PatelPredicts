db6785c8cbbe63645676263b96ba8fe8f872c96f
Harsh <harshpatel.backend.dev@gmail.com>
2026-06-13 23:42:31 +0530

    hotfixes

--- app/assets/javascripts/channels/consumer.js
+++ app/assets/javascripts/channels/consumer.js
@@ -1,7 +1,2 @@
-// app/javascript/channels/consumer.js
 import { createConsumer } from "@rails/actioncable"
-
-// This creates the WebSocket connection
-const consumer = createConsumer()
-
-export default consumer
+export default createConsumer()
