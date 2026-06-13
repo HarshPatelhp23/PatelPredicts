36a056c882f940826c2a322389a8ec1f3fa44cd6
Harsh <harshpatel.backend.dev@gmail.com>
2026-06-13 19:41:50 +0530

    hotfixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,47 +1,19 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-
-  # Code is not reloaded between requests.
   config.cache_classes = true
-
-  # Eager load code on boot. This eager loads most of Rails and
-  # your application in memory, allowing both threaded web servers
-  # and those relying on copy on write to perform better.
-  # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
-
-  # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
-  # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
-  # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  # CRITICAL CHANGE: Enable on-the-fly compilation
+  config.assets.compile = true  # Changed from false to true
 
-  # Disable serving static files from the `/public` folder by default since
-  # Apache or NGINX already handles this.
+  # Enable static file serving
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  config.public_file_server.enabled = true  # Force this for Render
 
-  # Compress CSS using a preprocessor.
-  # config.assets.css_compressor = :sass
-
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.asset_host = "http://assets.example.com"
-
-  # Specifies the header that your server uses for sending files.
-  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
-  # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
-
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
-
-  # Mount Action Cable outside main process or domain.
-  # config.action_cable.mount_path = nil
+  # Action Cable Configuration
   config.action_cable.mount_path = '/cable'
   config.action_cable.url = "wss://patelpredicts-4mw4.onrender.com/cable"
   config.action_cable.allowed_request_origins = [
@@ -50,83 +22,28 @@ Rails.application.configure do
   ]
   config.action_cable.disable_request_forgery_protection = true
 
-
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
-
-  # Include generic and useful information about system operation, but avoid logging too much
-  # information to avoid inadvertent exposure of personally identifiable information (PII).
+  # Other settings remain the same...
   config.log_level = :info
-
-  # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
-
-  # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
-
-  # Use a real queuing backend for Active Job (and separate queues per environment).
-  # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "live_auction_production"
-
   config.action_mailer.perform_caching = false
-
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
-
-  # Don't log any deprecations.
   config.active_support.report_deprecations = false
-
-  # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  # Use a different logger for distributed setups.
-  # require "syslog/logger"
-  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
-
   if ENV['RAILS_LOG_TO_STDOUT'].present?
-    logger           = ActiveSupport::Logger.new($stdout)
+    logger = ActiveSupport::Logger.new($stdout)
     logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  # GMAIL CONFIGURATION FOR SENDING MAILS
-  # config.action_mailer.smtp_settings = {
-  #   address:              ENV['SMTP_ADDRESS'] || Rails.application.credentials.dig(:smtp, :address),
-  #   port:                 ENV['SMTP_PORT'] || Rails.application.credentials.dig(:smtp, :port),
-  #   domain:               ENV['SMTP_DOMAIN'] || Rails.application.credentials.dig(:smtp, :domain),
-  #   user_name:            ENV['SMTP_USER_NAME'] || Rails.application.credentials.dig(:smtp, :user_name),
-  #   password:             ENV['SMTP_PASSWORD'] || Rails.application.credentials.dig(:smtp, :password),
-  #   authentication:       ENV['SMTP_AUTHENTICATION'] || Rails.application.credentials.dig(:smtp, :authentication),
-  #   enable_starttls_auto: ENV['SMTP_ENABLE_STARTTLS_AUTO'] || Rails.application.credentials.dig(:smtp, :enable_starttls_auto)
-  # }
-
-  # config.action_mailer.default_url_options = { host: 'patelpredicts-4mw4.onrender.com' }
-  # config.action_mailer.asset_host = 'https://patelpredicts-4mw4.onrender.com'
-
-
-  # Brevo CONFIGURATION FOR SENDING MAILS
-
   config.action_mailer.delivery_method = :brevo
-  
-  # Set default URL options for production
   config.action_mailer.default_url_options = { 
     host: 'patelpredicts-4mw4.onrender.com',
     protocol: 'https'
   }
-  # config.action_mailer.brevo_settings = {
-  #   api_key: ENV['BREVO_API_KEY'] || Rails.application.credentials.dig(:brevo, :api_key)
-  # }
   config.action_mailer.asset_host = 'https://patelpredicts-4mw4.onrender.com'
-  
-  # Email settings
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
 
-  # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
