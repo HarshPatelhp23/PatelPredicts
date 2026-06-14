2bf7ce5c17594b4ba80af28d7926ae6c774db50e
Harsh <harshpatel.backend.dev@gmail.com>
2026-06-14 22:01:36 +0530

    hotfixes

--- app/admin/auction_players.rb
+++ app/admin/auction_players.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register AuctionPlayer do
-  permit_params :name, :batting, :bowling, :sold, :points, :base_price, :note, :order, :sold, :category
+  permit_params :name, :batting, :bowling, :sold, :points, :base_price, :note, :order, :category
 
 
   index do
@@ -10,6 +10,7 @@ ActiveAdmin.register AuctionPlayer do
     column :name
     column :batting
     column :bowling
+    column :sold
     column :points
     column :category
     actions
@@ -17,18 +18,21 @@ ActiveAdmin.register AuctionPlayer do
 
 
   form do |f|
-    f.inputs do
+    f.inputs "Player Details" do
       f.input :name
       f.input :batting
       f.input :bowling
       f.input :points
       f.input :category, as: :select, collection: AuctionPlayer.categories.map { |key, value| [key.humanize, key] }
-
       f.input :base_price
       f.input :order
-      f.input :sold
       f.input :note
-      f.input :sold
+      
+      # Custom checkbox with explicit values
+      f.input :sold, as: :select, 
+              collection: [["No", false], ["Yes", true]], 
+              include_blank: false,
+              label: "Sold Status"
     end
     f.actions
   end
