2acc420d982bf214d63bd04c3baa64cc8d2f14e1
Harsh <harshpatel.backend.dev@gmail.com>
2026-06-14 12:35:21 +0530

    hotfixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -121,7 +121,7 @@ module ApplicationHelper
 
   def spl_team_name(username)
     # SPL_TEAM_MAPPING[username]
-    if user.username == 'Yash Agrawal' ? 'Shakuni Sena' : SVL_TEAM_MAPPING[username]
+    user.username == 'Yash Agrawal' ? 'Shakuni Sena' : SVL_TEAM_MAPPING[username]
   end
 
   def spl_team_logo(username)
